7c1f3a591dd560806ec5cbf9ff9ce8982581b8bc
AlphaTech <alphat3ch@icloud.com>
2015-06-19 03:21:08 -0400

    Update help.js

--- modules/help.js
+++ modules/help.js
@@ -4,7 +4,7 @@ var settings = require("../config.js").settings;
 
 exports.module = function() {
 	this.onCommand_help = function(nick, command) {
-		this.channel.say(settings.globalNick + " by Dirbaio, Nina, and LifeMushroom. Running on Node.js " + process.versions.node + " (" + os.type() + " " + os.release() + " " + os.arch() + ").");
+		this.channel.say(settings.globalNick + " v 1.1.0 by Dirbaio, Nina, and LifeMushroom. Running on Node.js " + process.versions.node + " (" + os.type() + " " + os.release() + " " + os.arch() + ").");
 		this.channel.say("For a list of available commands, check https://github.com/LifeMushroom/Modular-Node.js-IRC-Bot/blob/master/COMMANDS.md");
 	}
 }
